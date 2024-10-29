9fbca036c3e2c3c0dd2739d1e86ec45f7281144b
Tanish Vijayvargiya <162412801+tanishdt@users.noreply.github.com>
2024-10-29 14:20:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,3 @@ cd cpuminer-multi
 
 # Build the miner
 ./build.sh
-
-# Run cpuminer with specified parameters
-./cpuminer --algo sha256d --url stratum+tcp://public-pool.io:21496 --user bc1qmffl7e9m9hyar49wda34k6trgx08u6v48gwedq.random_git --pass x --threads 8
